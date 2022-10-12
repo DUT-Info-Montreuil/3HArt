e19c6dc4f0d5ac700cdc27155ed7f93576e008ee
Fabrice <fabrice.amegadjen@gmail.com>
2022-10-12 16:28:28 +0200

    mise à jour

--- modules/mod_accueil/modele_accueil.php
+++ modules/mod_accueil/modele_accueil.php
@@ -8,18 +8,8 @@
 		public function connexion(){
 			session_start();
 			$_SESSION['login'] = "Gilgamesh";
-			//header('Location: index.php?action=bienvenue&module=connexion');
 			echo "Vous êtes connecté en tant que : " . $_SESSION['login'] ;
-			/* if (isset($_POST['login'])){
-				$query = "SELECT * FROM users WHERE login=$_POST[login]and password=.hash(sha256,$_POST[password] ).";
-			    $result = mysqli_query($conn,$query);
-			    $rows = mysqli_num_rows($result);
-			    if($rows==1){
-				    $_SESSION['login'] = $login;
-			    }else{
-				  $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
-			    }
-			} */
+			
 		}	
 		
 		public function deconnexion(){
@@ -29,11 +19,9 @@
 				session_destroy();
 				unset($_SESSION);
 				echo "Vous êtes deconnecté";
-				//header('Location: index.php?action=bienvenue&module=connexion');
 			}
 			else{
 				echo "Vous n'êtes pas connecté";
-				//eader('Location: index.php?action=bienvenue&module=connexion');
 			}
 			
 		}
