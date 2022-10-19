cf6126c968979953c48daa3c238eb1e8f9e04ba9
Ngoc Minh <xixicutee@gmail.com>
2022-10-19 09:34:07 +0200

    Delete mod decouvrir

--- modules/mod_decouvrir/modele_decouvrir.php
+++ modules/mod_decouvrir/modele_decouvrir.php
@@ -3,6 +3,27 @@
 include_once('connexion.php');
 
     class ModeleDecouvrir extends Connexion{
-        
+
+        public function __construct(){
+        }
+
+        public function getListe() {  
+
+            $req = self::$bdd->prepare("SELECT * FROM ");
+            $req->execute();
+            $tab = $req->fetchAll();
+
+            return $tab;
+        }
+
+
+
+        public function getDetails($id) {
+            $req = self::$bdd->prepare("SELECT * FROM equipe WHERE id = ?");
+            $req->execute(array($id));
+            $t = $req->fetch();
+            
+            return $t;
+        }
     }
 ?>
--- modules/mod_decouvrir/vue_decouvrir.php
+++ modules/mod_decouvrir/vue_decouvrir.php
@@ -1,5 +1,26 @@
 <?php
     class VueDecouvrir{
         
+        public function __construct() {
+        }
+        public function bienvenue() {
+            echo 'bienvenue';
+        }
+        public function affiche_liste($tab) {
+            foreach($tab as $t) {
+                echo '<a href ="index.php?module=equipes&action=details&id=' . $t['id'] . '">' . $t['id'] . '.' . $t['nom'] . '<br>'.'</a>';
+            }
+        }
+        public function menu() {
+           echo "<a href =\"index.php?module=equipes&action=bienvenue\">Bienvenue <br></a>";
+           echo "<a href =\"index.php?module=equipes&action=liste\">Liste <br></a>"; 
+        }
+        
+        public function affiche_details($tab) {
+                echo $tab['description'] . '<br>';
+                echo "Pays : " . $tab['pays'] . '<br>';
+                echo "Année de création : " . $tab['annee_creation'] . '<br>';
+                echo "Logo : <img src ='" . $tab['logo'] . "'>";
+            }
         }
 ?>
