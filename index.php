f37e620255e6b80a41a825f6335cca4913ae9f9a
Ngoc Minh <xixicutee@gmail.com>
2022-10-19 16:03:55 +0200

    base de pageDecouvrir

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-
     require_once("modules/mod_accueil/mod_accueil.php");
+    include_once('modules/mod_decouvrir/mod_decouvrir.php');
+
     #require_once('decouvrir/mod_decouvrir.php');
 
     echo("<HEADER>
@@ -15,13 +16,17 @@
         case 'accueil':
             $mod = new mod_accueil();
             $mod->exec();
-            echo ("<a href=\"index.php?module=decouverte\">Découverte</a>");
+            echo ("<a href=\"index.php?module=decouvrir\">Découverte</a>");
             break;
 
-        case 'decouverte':
-            # code...
+        case 'decouvrir':
+            $mod = new ModDecouvrir();
+            $mod->exec();
             break;
     }
+
+    function 
+
     echo("<FOOTER>
             <p>
                 <a href = index.php>
--- modules/mod_decouvrir/mod_decouvrir.php
+++ modules/mod_decouvrir/mod_decouvrir.php
@@ -1,25 +1,49 @@
+
+
 <?php
 
 include_once('cont_decouvrir.php');
 include_once('modele_decouvrir.php');
 
-Connexion::initConnexion();
 
-$controleur = new ControleurDecouvrir();
+class ModDecouvrir{
+    private $controleur;
 
-$controleur->menu();
+    function __construct(){
+        $this->controleur = new ControleurDecouvrir();
+    }
+    //Connexion::initConnexion();
 
-switch($controleur->get_action()) {
-    case "details":
-        $controleur->detailsUsers();
-        break;
-    case "bienvenue":
-        $controleur->bienvenue();
-        break;
-    case "liste":
-        $controleur->liste();
-        break;
+    public function exec(){
+        $this->controleur->exec();
+    
+
+        if (isset($_GET['action'])){
+            switch($_GET['action']) {
+                case "details":
+                    $this->controleur->detailUser();
+                    break;
+        }
+        }
+
+    }
+
+    
+
+    public function searching(){
+        switch ($variable) {
+            case 'value':
+                # code...
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+
 }
+
 
 ?>
 
