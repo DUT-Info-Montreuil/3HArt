469d83b17f94701fd6931152244c11e069e8914e
ShimmerPlume <94871035+ShimmerPlume@users.noreply.github.com>
2022-12-11 19:29:18 +0100

    rebase

--- modules/mod_accueil/cont_accueil.php
+++ modules/mod_accueil/cont_accueil.php
@@ -14,92 +14,35 @@ require_once("vue_accueil.php");
         }
          
         function exec(){
-            $this->vue->menu();
-            echo "<br>";
-            echo $this->action;
-            switch($this->action) { 
-                case "bienvenue":
-                    $this->vue->menu();
-                    echo "<br>";
-                    break;
-
-                case "inscription":
-                    $this->vue->formulaireInscription();
-                    break;
-
-                case "connexion";
-                    $this->vue->menu();
-                    echo "<br>";
-                    $this->vue->afficher($this->modele->connexion());
-                    break;
-				
-				case "deconnexion";
-                    $this->vue->menu();
-                    echo "<br>";
-                    $this->vue->afficher($this->modele->deconnexion());
-                    break;
-					
-				case "ajout";
-                    $this->modele->ajout($_GET["login"],$_GET["password"]);
-                    break;
-
-                case "maChaine";
-                    if($this->modele->estConnecter()){
-                        $this->vue->maChaine(TRUE);
-                    }
-                    else{
+            echo ($this->vue->menu());
+            if (isset($_GET['action'])) {
+                switch($_GET['action']) { 
+                    case "bienvenue":
                         $this->vue->menu();
                         echo "<br>";
-                        $this->vue->pasConnecter();
-                    }
-                    break;
+                        break;
 
-                case "commenter";
-                    $this->vue->commenter();
-                    break;
+                    case "inscription":
+                        $this->vue->formulaireInscription();
+                        break;
 
-                case "posterCommentaire";
-                    $this->modele->posterCommentaire($_POST["commentaire"]);
-                    $this->vue->maChaine(TRUE);
-                    break;
+                    case "connexion";
+                        $this->vue->menu();
+                        echo "<br>";
+                        $this->vue->afficher($this->modele->connexion());
+                        break;
+                    
+                    case "deconnexion";
+                        $this->vue->menu();
+                        echo "<br>";
+                        $this->vue->afficher($this->modele->deconnexion());
+                        break;
+                        
 
-                case "lireCommentaire";
-                    $this->vue->maChaine(FALSE);
-                    $commentaire = $this->modele->lireCommentaire();
-                    if($commentaire != -1){
-                        $this->vue->afficherCommentaires($commentaire);
-                    }
-                    else{
-                        $this->vue->afficher("Pas de commentaire à afficher :)");
-                    }
-                    echo "<br>";
-                    $this->vue->afficheVosImage();
-                    break;
-
-                case "ajoutImage";
-                    $this->vue->image();
-                    break;
-
-                case "supprimerImage";
-                    $this->vue->formulaireSuppression();
-                    break;
-
-                case "suppression";
-                    $this->vue->afficher($this->modele->suppression($_POST["id"]));
-                    $this->vue->afficher($this->modele->suppression($_POST["id"]));
-                    echo "test";
-                    $this->modele->suppression($_POST["id"]);
-                    break;
-
-
-                case "uploadImage";
-                    $this->vue->afficher($this->modele->upload());
-                    break;
-
-                default:
-                    echo "erreur : " . $this->action;
-                    break;
-                
+                    default:
+                        echo "erreur : " . $this->action;
+                        break;
+                }    
             }
         }
         
